c3d385f0cb63aac9412513792cb46298a9012c73
khushboogoyal2023-web <khushboogoyal2023@gmail.com>
2026-06-09 22:38:39 +0530

    Create label.jsx

---
+++ src / components / ui /label.jsx
@@ -0,0 +1,16 @@
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
+import { cva } from "class-variance-authority";
+
+import { cn } from "@/lib/utils"
+
+const labelVariants = cva(
+  "text-xs sm:text-sm font-heading font-black leading-none tracking-wide text-foreground/95 peer-disabled:cursor-not-allowed peer-disabled:opacity-40 select-none"
+)
+
+const Label = React.forwardRef(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
+))
+Label.displayName = LabelPrimitive.Root.displayName
+
+export { Label }
