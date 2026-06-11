b43d2b555d3f4123a3c0e6a14b229d35d43286cc
khushboogoyal2023-web <khushboogoyal2023@gmail.com>
2026-06-11 08:58:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/NKG-MATH/', // यह आपके गिटहब प्रोजेक्ट का नाम है
+  base: '/NKG-MATH/', 
 })
