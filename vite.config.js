57c9298257a2d71964b903d6c2eac52033cdb0f5
khushboogoyal2023-web <khushboogoyal2023@gmail.com>
2026-06-11 18:52:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NKG-MATH/', 
+  base: '/NKG-MATH/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
